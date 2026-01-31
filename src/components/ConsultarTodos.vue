a583e12482291a5d7a52cebdae5fea6d2f62fb31
jisantamariar1 <jisantamariar1@uce.edu.ec>
2026-01-30 19:44:21 -0500

    correcion

--- src/components/ConsultarTodos.vue
+++ src/components/ConsultarTodos.vue
@@ -2,60 +2,66 @@
     <h1>Consultar Todos</h1>
     <button v-on:click="consultarTodos">Consultar Todos</button>
     <table class="tabla">
-        <tr>
-            <th>ID</th>
-            <th>Nombre</th>
-            <th>Apellido</th>
-            <th>Fecha de Nacimiento</th>
-            <th>Genero</th>
-            <th>Links</th>
-        </tr>
-        <tr v-for="(item, index) in json" :key="index">
-            <td>{{ item.id }}</td>
-            <td>{{ item.nombre }}</td>
-            <td>{{ item.apellido }}</td>
-            <td>{{ item.fechaNacimiento }}</td>
-            <td>{{ item.genero }}</td>
-            <td>
-                <ul>
-                    <li v-for="(link, linkIndex) in item.links" :key="linkIndex">
-                        <a :href="link.href">{{ link.rel }}</a>
-                    </li>
-                </ul>
-            </td>
+        <thead>
+            <tr>
 
-        </tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Apellido</th>
+                <th>Fecha de Nacimiento</th>
+                <th>Genero</th>
+                <th>Links</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="(item, index) in json" :key="index">
+                <td>{{ item.id }}</td>
+                <td>{{ item.nombre }}</td>
+                <td>{{ item.apellido }}</td>
+                <td>{{ item.fechaNacimiento }}</td>
+                <td>{{ item.genero }}</td>
+                <td>
+                    <ul>
+                        <li v-for="(link, linkIndex) in item.links" :key="linkIndex">
+                            <a :href="link.href">{{ link.rel }}</a>
+                        </li>
+                    </ul>
+                </td>
+
+            </tr>
+        </tbody>
     </table>
-   
-    </template>
 
-    <script>
-    import { consultarTodosFacade } from '@/clients/MatriculaClient';
-    export default {
-        methods: {
-            async consultarTodos() {
-                const resp = await consultarTodosFacade();
-                console.log(resp);
-                this.json=resp;
-            }
-        },
-        data() {
-            return {
-                json: null
-            };  
+</template>
+
+<script>
+import { consultarTodosFacade } from '@/clients/MatriculaClient';
+export default {
+    methods: {
+        async consultarTodos() {
+            const resp = await consultarTodosFacade();
+            console.log(resp);
+            this.json = resp;
         }
-
+    },
+    data() {
+        return {
+            json: null
+        };
     }
-    </script>
 
-    <style>
+}
+</script>
+
+<style>
 .tabla {
     margin-top: 20px;
     border-collapse: collapse;
     width: 100%;
 }
 
-.tabla th, .tabla td {
+.tabla th,
+.tabla td {
     border: 1px solid #ddd;
     padding: 8px;
 }
